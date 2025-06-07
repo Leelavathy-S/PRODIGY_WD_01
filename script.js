6b59ce5147f496331b5da188bc695de03dceaec2
Leelavathy-S <leelavathy.s284@gmail.com>
2025-06-07 10:39:26 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Detect scroll and add class to navbar
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
